c4eba2387396381fbe5c8f4b161f29d294a86c6c
Adi Cutitoiu <acutitoiu2@gmail.com>
2018-10-16 22:05:26 +0300

    Add body parser and json web token packages

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const mainRouter = require('./src/routers/mainRouter');
 
 const server = express();
 
+server.use(bodyParser.json());
 server.use('/', mainRouter);
 
 const port = process.env.PORT || 3000;
