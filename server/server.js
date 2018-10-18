b49461156ecd9a852762870555f0841a76d98732
Adi Cutitoiu <acutitoiu2@gmail.com>
2018-10-18 10:54:32 +0300

    Create routes only if the db connection is successful

--- server/server.js
+++ server/server.js
@@ -4,20 +4,18 @@ const mongoose = require('mongoose');
 const mainRouter = require('./src/routers/mainRouter');
 
 mongoose.connect('mongodb://onlinejudge:caphyon2018@ds021016.mlab.com:21016/onlinejudge', (err) => {
-  if(err) {
+  if (err) {
     console.log('Database connection failed');
     throw err;
   }
-  
-  console.log('Connection to datbase is successful');
-});
 
-const server = express();
+  const server = express();
 
-server.use(bodyParser.json());
-server.use('/', mainRouter);
+  server.use(bodyParser.json());
+  server.use('/', mainRouter);
 
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+  const port = process.env.PORT || 3000;
+  server.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+  });
 });
