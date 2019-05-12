dc0147a5d1d0fb6b20ddc4b9ab3085584a2ef558
Adi Cutitoiu <acutitoiu2@gmail.com>
2019-05-12 20:29:42 +0300

    Fix back button not show state undefined

--- client/src/components/Toolbar.vue
+++ client/src/components/Toolbar.vue
@@ -1,9 +1,8 @@
 <template>
   <v-toolbar flat fixed prominent app>
-    <v-toolbar-title class="text-uppercase">
-      <span class="text-lowercase red--text text--darken-1">&lt;code</span>
-      <span class="text-lowercase">br /&gt;</span>
-    </v-toolbar-title>
+    <v-btn flat @click="onBack" v-show="showBack">
+      <v-icon large>arrow_back</v-icon>
+    </v-btn>
     <v-spacer></v-spacer>
     <v-btn flat @click="signout" router :to="''">
       <span>Signout</span>
@@ -13,14 +12,26 @@
 </template>
 
 <script>
+
 export default {
   name: "Toolbar",
+  mounted: function() {
+    setInterval(() => {
+      this.showBack =
+        (this.$router.currentRoute.fullPath.match(/\//g) || []).length > 1;
+    }, 100);
+  },
   methods: {
     signout: function() {
       this.$emit("signout");
+    },
+    onBack: function() {
+      this.$router.back();
     }
   },
-  data: () => ({})
+  data: () => ({
+    showBack: false
+  })
 };
 </script>
 
